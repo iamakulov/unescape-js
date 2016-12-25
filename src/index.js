1e7a605071240e9059774f9b67a37d5f72fdee7f
Ivan Akulov <iamakulov@users.noreply.github.com>
2016-12-25 21:39:40 +0300

    Simplify the regex + document it
    Simplify: duplicate the `u` part between the first and the second alternative. This reduces the regex depth.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import 'string.fromcodepoint';
 
-const jsEscapeRegex = /\\(u(\{([0-9A-Fa-f]+)\}|[0-9A-Fa-f]{4})|x([0-9A-Fa-f]{2})|(\d{3})|['"tbrnfv0/\\])/g;
+/**
+ * \\ - matches the backslash which indicates the beginning of an escape sequence
+ * (
+ *   u\{([0-9A-Fa-f]+)\} - first alternative; matches the variable-length hexadecimal escape sequence (\u{ABCD0})
+ * |
+ *   u([0-9A-Fa-f]{4}) - second alternative; matches the 4-digit hexadecimal escape sequence (\uABCD)
+ * | 
+ *   x([0-9A-Fa-f]{2}) - third alternative; matches the 2-digit hexadecimal escape sequence (\xA5)
+ * | 
+ *   (\d{3}) - fourth alternative; matches the 3-digit octal escape sequence (\512)
+ * | 
+ *   (['"tbrnfv0/\\]) - fifth alternative; matches the special escape characters (\t, \n and so on)
+ * )
+ */
+const jsEscapeRegex = /\\(u\{([0-9A-Fa-f]+)\}|u([0-9A-Fa-f]{4})|x([0-9A-Fa-f]{2})|(\d{3})|(['"tbrnfv0/\\]))/g;
 
 const usualEscapeSequences = {
     '0': '\0',
