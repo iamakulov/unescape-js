0820a9592e4af88bb093ff0e8a6ff31d1da823ad
Robert Hafner <tedivm@tedivm.com>
2017-11-18 09:05:36 -0800

    Add python style unicode support (`\U0001F3B5`)

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import 'string.fromcodepoint';
  *   u\{([0-9A-Fa-f]+)\} - first alternative; matches the variable-length hexadecimal escape sequence (\u{ABCD0})
  * |
  *   u([0-9A-Fa-f]{4}) - second alternative; matches the 4-digit hexadecimal escape sequence (\uABCD)
- * | 
+ * |
  *   x([0-9A-Fa-f]{2}) - third alternative; matches the 2-digit hexadecimal escape sequence (\xA5)
- * | 
+ * |
  *   ([1-7][0-7]{0,2}|[0-7]{2,3}) - fourth alternative; matches the up-to-3-digit octal escape sequence (\5 or \512)
- * | 
+ * |
  *   (['"tbrnfv0\\]) - fifth alternative; matches the special escape characters (\t, \n and so on)
+ * |
+ *   \U([0-9A-Fa-f]+) - first alternative; matches the variable-length hexadecimal escape sequence (\u{ABCD0})
  * )
  */
-const jsEscapeRegex = /\\(u\{([0-9A-Fa-f]+)\}|u([0-9A-Fa-f]{4})|x([0-9A-Fa-f]{2})|([1-7][0-7]{0,2}|[0-7]{2,3})|(['"tbrnfv0\\]))/g;
+const jsEscapeRegex = /\\(u\{([0-9A-Fa-f]+)\}|u([0-9A-Fa-f]{4})|x([0-9A-Fa-f]{2})|([1-7][0-7]{0,2}|[0-7]{2,3})|(['"tbrnfv0\\]))|\U([0-9A-Fa-f]+)/g;
 
 const usualEscapeSequences = {
     '0': '\0',
@@ -33,7 +35,7 @@ const fromHex = (str) => String.fromCodePoint(parseInt(str, 16));
 const fromOct = (str) => String.fromCodePoint(parseInt(str, 8));
 
 export default (string) => {
-    return string.replace(jsEscapeRegex, (_, __, varHex, longHex, shortHex, octal, specialCharacter) => {
+    return string.replace(jsEscapeRegex, (_, __, varHex, longHex, shortHex, octal, specialCharacter, python) => {
         if (varHex !== undefined) {
             return fromHex(varHex);
         } else if (longHex !== undefined) {
@@ -42,6 +44,8 @@ export default (string) => {
             return fromHex(shortHex);
         } else if (octal !== undefined) {
             return fromOct(octal);
+        } else if (python !== undefined) {
+            return fromHex(python);
         } else {
             return usualEscapeSequences[specialCharacter];
         }
